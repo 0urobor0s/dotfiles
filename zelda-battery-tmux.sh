3a1415c4a2b619d7f7fa63fe64d9db5c7cfb0454
0urobor0s <lhome.gamer@gmail.com>
2017-10-15 22:01:16 +0100

    Need to see that percentage %

--- zelda-battery-tmux.sh
+++ zelda-battery-tmux.sh
@@ -13,11 +13,11 @@ if (( $+commands[pmset] )) ; then
   battery="$(pmset -g ps | awk 'NR==2' | perl -pe 's/.*?(\d+)%.*/\1/')"
 
   if [[ $battery -lt 25 ]]; then
-    echo "#[blink]❤ #[noblink]♡ ♡ "
+    echo "$battery% #[blink]❤ #[noblink]♡ ♡ "
   elif [[ $battery -lt 50 ]]; then
-    echo "❤ ♡ ♡ "
+    echo "$battery% ❤ ♡ ♡ "
   elif [[ $battery -lt 75 ]]; then
-    echo "❤ ❤ ♡ "
+    echo "$battery% ❤ ❤ ♡ "
   else
     echo "❤ ❤ ❤ "
   fi
